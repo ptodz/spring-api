de6d06ffbb97e99f6df04f42caf9cbe127f32fc4
mk <ptodztel@gmail.com>
2026-06-06 11:11:43 +0200

    secret fix

--- src/main/java/com/codewithmosh/store/services/JwtService.java
+++ src/main/java/com/codewithmosh/store/services/JwtService.java
@@ -12,13 +12,14 @@ public class JwtService {
 
     @Value("${spring.jwt.secret}")
     private String secret;
+
     public String generateToken(String email) {
         final long tokenExpiration = 86400;
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
     }
 }
