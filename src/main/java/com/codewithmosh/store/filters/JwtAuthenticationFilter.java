073718fbc68ec2ab4cb89d17ed5b23cd71e90054
mk <ptodztel@gmail.com>
2026-06-07 20:41:35 +0200

    jwt auth filter fix

--- src/main/java/com/codewithmosh/store/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/codewithmosh/store/filters/JwtAuthenticationFilter.java
@@ -21,21 +21,23 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var authToken = request.getHeader("Authorization");
-        if (authToken != null && authToken.startsWith("Bearer ")) {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        var token = authToken = authToken.replace("Bearer ", "");
+        var token = authHeader.replace("Bearer ", "");
         if (!jwtService.validateToken(token)) {
             filterChain.doFilter(request, response);
             return;
         }
 
         var authentication = new UsernamePasswordAuthenticationToken(
-                jwtService.getUserIdFromToken(authToken),
+                jwtService.getUserIdFromToken(token),
                 null,
                 null
         );
